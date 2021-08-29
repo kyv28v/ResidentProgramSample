ee7169b344daf0ca90690fe000347f7ed0a37ea9
kazu-y <kazu-y@softkaihatsu.co.jp>
2021-08-29 11:10:49 +0900

    first commit.

---
+++ App.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ResidentProgramSample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private Thread thread;
+        private bool aborted = false;
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+
+        // 起動
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Debug.WriteLine("OnStartup");
+
+            base.OnStartup(e);
+
+            var icon = GetResourceStream(new Uri("icon.ico", UriKind.Relative)).Stream;
+
+            var menu = new System.Windows.Forms.ContextMenuStrip();
+            menu.Items.Add("終了", null, Exit_Click);
+
+            notifyIcon = new System.Windows.Forms.NotifyIcon
+            {
+                Visible = true,
+                Icon = new System.Drawing.Icon(icon),
+                Text = "ResidentProgramSample",
+                ContextMenuStrip = menu
+            };
+
+            notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(NotifyIcon_Click);
+
+            // 定周期処理の開始
+            this.thread = new Thread(intervalProcess);
+            this.thread.Start();
+        }
+
+        // アイコンクリック
+        private void NotifyIcon_Click(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                ResidentProgramSample.MainWindow.GetInstance().Show();
+            }
+        }
+
+        // 終了
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            // 定周期処理をアボート
+            this.aborted = true;
+            // タスクトレイのアイコンを削除（これがないとアイコンが残り続ける）
+            notifyIcon.Dispose();
+            // アプリ終了
+            Shutdown();
+        }
+
+        // 定周期処理
+        private void intervalProcess(){
+            // アボートされるまで繰り返す
+            var index = 1;
+            while(!this.aborted){
+                Debug.WriteLine("debug:" + index) ;
+                index++;
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ResidentProgramSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static MainWindow _Instance;
+        public static MainWindow GetInstance()
+        {
+            if (_Instance == null)
+            {
+                _Instance = new MainWindow();
+            }
+            return _Instance;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // 設定値を初期表示する
+            Loaded += (s, e) => {
+                var textInput = "テストテキスト";
+                var folderPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Downloads\";
+                var spinText = 5;
+
+                TextInputText.Text = textInput;
+                FolderPathText.Text = folderPath;
+                SpinText.Child.Text = spinText.ToString();
+            };
+        }
+
+
+        protected virtual void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // 閉じる処理をキャンセルして非表示にする
+            e.Cancel = true;
+            this.Hide();
+        }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Apply"
+                + "\n"+ "テキスト＝" + TextInputText.Text
+                + "\n"+ "フォルダ＝" + FolderPathText.Text
+                + "\n"+ "スピン＝" + SpinText.Child.Text
+            );
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        
+        private void FolderPathButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var fbd = new System.Windows.Forms.FolderBrowserDialog()
+            {
+                Description = "フォルダを選択してください",
+                SelectedPath = FolderPathText.Text,
+            })
+            {
+                // OKが押されずに終わった場合はキャンセルされている
+                if (fbd.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                {
+                    return;
+                }
+
+                // SelectedPathで選択されたフォルダを取得する
+                //MessageBox.Show($"{fbd.SelectedPath}を選択しました");
+                FolderPathText.Text = fbd.SelectedPath;
+            }
+        }
+    }
+}
